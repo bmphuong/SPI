333c6c7ab491eed67f51eef7ed73732b743fa6dc
unknown <bmphuongdn1412@gmail.com>
2024-03-05 15:47:01 +0700

    update

--- SPI/Core/Src/main.c
+++ SPI/Core/Src/main.c
@@ -31,7 +31,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define TURN 30
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -42,25 +42,48 @@
 /* Private variables ---------------------------------------------------------*/
 SPI_HandleTypeDef hspi1;
 
+TIM_HandleTypeDef htim2;
+
 /* USER CODE BEGIN PV */
 uint8_t PSX_RX[8];
 uint8_t PSX_TX[2] = {
 		0x01, 0x42
 };
-
+enum state
+{
+	FORWARD,
+	FOR_LEFT,
+	FOR_RIGHT,
+	BACKWARD,
+	BAC_LEFT,
+	BAC_RIGHT,
+	LEFT_ROUND,
+	RIGHT_ROUND,
+	ZERO
+};
+enum state state_check(uint8_t arr[8]);
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_SPI1_Init(void);
+static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+void forward();
+void left();
+void right();
+void backward();
+void forward_left();
+void forward_right();
+void backward_left();
+void backward_right();
+void stop_case();
 /* USER CODE END 0 */
 
 /**
@@ -92,8 +115,13 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_SPI1_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
+  HAL_TIM_PWM_Start(&htim2,TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim2,TIM_CHANNEL_2);
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, 1);
+
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -103,10 +131,52 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	 HAL_Delay(5);
 	 HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
 	 HAL_SPI_TransmitReceive(&hspi1, PSX_TX, PSX_RX, 8, 10);
 	 HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
-	 HAL_Delay(100);
+	 switch (state_check(PSX_RX))
+	 {
+		 case FORWARD :
+			 forward();
+			 break;
+
+		 case FOR_LEFT :
+			forward_left();
+			break;
+
+		 case FOR_RIGHT :
+			forward_right();
+			break;
+
+		 case BACKWARD :
+			backward();
+			break;
+
+		 case BAC_LEFT :
+			backward_left();
+			break;
+
+		 case BAC_RIGHT :
+			backward_right();
+			break;
+
+		 case RIGHT_ROUND :
+		 			right();
+		 			break;
+
+		 case LEFT_ROUND :
+		 			left();
+		 			break;
+
+		 case ZERO:
+			stop_case();
+			break;
+
+		 default:
+			 break;
+	 }
+
   }
   /* USER CODE END 3 */
 }
@@ -126,7 +196,9 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -136,7 +208,7 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
@@ -186,6 +258,69 @@ static void MX_SPI1_Init(void)
 }
 
 /**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 7;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 99;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+  HAL_TIM_MspPostInit(&htim2);
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -197,11 +332,22 @@ static void MX_GPIO_Init(void)
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA2 PA3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PB0 */
   GPIO_InitStruct.Pin = GPIO_PIN_0;
@@ -215,7 +361,90 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
+enum state state_check(uint8_t arr[8])
+{
+	uint8_t forward_flag = 0;
+	uint8_t backward_flag = 0;
+	uint8_t left_flag = 0;
+	uint8_t right_flag = 0;
+	if (arr[3] == 127 || arr[3] == 111 || arr[3] == 63) left_flag = 1;
+	if (arr[3] == 223 || arr[3] == 207 || arr[3] == 159) right_flag = 1;
+	if (arr[4] == 239 || arr[4] == 111 || arr[4] == 207) forward_flag = 1;
+	if (arr[4] == 159 || arr[4] == 191 || arr[4] == 63) backward_flag = 1;
+	if (forward_flag && left_flag) return FOR_LEFT;
+	else
+		if (forward_flag && right_flag) return FOR_RIGHT;
+		else if (forward_flag) return FORWARD;
+		else if (backward_flag && left_flag) return BAC_LEFT;
+		else if (backward_flag && right_flag) return BAC_RIGHT;
+		else if (backward_flag) return BACKWARD;
+		else if (left_flag) return LEFT_ROUND;
+		else if (right_flag) return RIGHT_ROUND;
+		else return ZERO;
+}
+void forward()
+{
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 1);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, 0);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, 0);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_2, 100);
+}
+void left()
+{
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 0);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, 100);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, 0);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_2, 100);
+}
+void right()
+{
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 1);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, 0);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, 1);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_2, 0);
+}
+void backward()
+{
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 0);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, 100);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, 1);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_2, 0);
+}
+void forward_left()
+{
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 1);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, 100 - TURN);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, 0);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_2, 100);
+}
+void forward_right()
+{
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 1);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, 0);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, 0);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_2, TURN);
+}
+void backward_left()
+{
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 0);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, TURN);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, 1);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_2, 0);
+}
+void backward_right()
+{
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 0);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, 100);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, 1);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_2, 100 - TURN);
+}
+void stop_case()
+{
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 0);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_1, 0);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, 0);
+	__HAL_TIM_SetCompare(&htim2, TIM_CHANNEL_2, 0);
+}
 /* USER CODE END 4 */
 
 /**
